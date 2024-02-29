74f239513097d66052dc007e9a1ea44f2630db49
Aborisade <bobleeswaggs123@gmail.com>
2024-02-29 23:45:56 +0100

    using a library for the form validation

--- src/components/Movieform1.js
+++ src/components/Movieform1.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import * as yup from 'yup'
 import {useForm} from "react-hook-form"
 
 const Movieform1 = () => {
@@ -6,6 +7,13 @@ const Movieform1 = () => {
     const onSubmit = (data) => {
         console.log(data)
     }
+    const schema = yup.object().shape({
+        fullName : yup.string().required(),
+        email : yup.string().email().required(),
+        age: yup.number().positive().integer().min(18).required(),
+        password: yup.string().min(4).max(20).required(),
+        confirmPassword : yup.string().oneOf([yup.ref("password"), null]).required()
+    })
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
         <input type='text' placeholder='Fullname...' {...register("fullName")} />
